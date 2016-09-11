849aba5ef8bf6dcd146706a96c8610da742bb004
Takashi Masuda <masutaka.net@gmail.com>
2016-09-11 21:41:58 +0900

    Refactor

--- ci-bundle-update.rb
+++ ci-bundle-update.rb
@@ -1,18 +1,34 @@
 require 'circleci'
 
-if ENV['EXEC_DAYS'] &&
-   ! ENV['EXEC_DAYS'].split(',').include?(Time.now.strftime('%a'))
-  exit 0
+class CiBundleUpdate
+  def initialize(circleci_token, exec_days)
+    CircleCi.configure do |config|
+      config.token = circleci_token
+    end
+
+    @exec_days = exec_days
+  end
+
+  def build(username, reponame, branch)
+    return if skip?
+
+    response = CircleCi::Project.build_branch(
+      username, reponame, branch, {},
+      build_parameters: { BUNDLE_UPDATE: true }
+    )
+  end
+
+  private
+
+  attr_reader :exec_days
+
+  def skip?
+    exec_days &&
+      ! exec_days.split(',').include?(Time.now.strftime('%a'))
+  end
 end
 
-CircleCi.configure do |config|
-  config.token = ENV['CIRCLECI_TOKEN']
+if $0 == __FILE__
+  ci_bundle_update = CiBundleUpdate.new(ENV['CIRCLECI_TOKEN'], ENV['EXEC_DAYS'])
+  ci_bundle_update.build(ENV['GITHUB_USERNAME'], ENV['GITHUB_REPONAME'], ENV['BRANCH'])
 end
-
-CircleCi::Project.build_branch(
-  ENV['GITHUB_USERNAME'],
-  ENV['GITHUB_REPONAME'],
-  ENV['BRANCH'],
-  {},
-  build_parameters: { BUNDLE_UPDATE: true }
-)
