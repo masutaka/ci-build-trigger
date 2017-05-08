224ee7961d795ec405b68ed93d2c8a4fdfcf7846
Takashi Masuda <masutaka@feedforce.jp>
2017-05-08 09:54:51 +0900

    Fixup 478a056c

--- bulk-sample.rb
+++ bulk-sample.rb
@@ -3,6 +3,7 @@ require_relative 'ci-bundle-update'
 exec_days = ENV['EXEC_DAYS']
 github_username = ENV['GITHUB_USERNAME']
 branch = ENV['BRANCH']
+extra_build_parameters = ENV['EXTRA_BUILD_PARAMETERS']
 
 # $TRIGGERS :: <CIRCLECI_TOKEN>:<GITHUB_REPONAME>,<CIRCLECI_TOKEN>:<GITHUB_REPONAME>,...
 ENV['TRIGGERS'].split(',').each do |trigger|
@@ -10,6 +11,6 @@ ENV['TRIGGERS'].split(',').each do |trigger|
 
    puts "GitHub repository is #{github_username}/#{github_reponame}"
 
-   ci_bundle_update = CiBundleUpdate::CircleCi.new(circleci_token, exec_days)
+   ci_bundle_update = CiBundleUpdate::CircleCi.new(circleci_token, exec_days, extra_build_parameters)
    ci_bundle_update.build(github_username, github_reponame, branch)
 end
